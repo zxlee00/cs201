0af71ed0ede60210b9a3f8025daca9c215a7a167
ChongJun123 <cjchew.2020@scis.smu.edu.sg>
2021-11-01 10:56:48 +0800

    done with a2a and a2h
    a2a and a2h done
    tests written

--- step2.java
+++ step2.java
@@ -3,7 +3,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-import LinkedBinaryTree.Node;
+import java.util.List;
+
 import entity.Business;
 import entity.Review;
 
@@ -13,8 +14,10 @@ import entity.Review;
 * - Args: List<Reviews> + (List, LL or BT)
 * 
 */
+// return userID linked to numOfSusReviews
 public class step2 {
-    // Returns Map<userID, numOfSusReviews>
+
+    // Returns Map<String userID, Integer numOfSusReviews>
     public Map<String, Integer> ArrayListToHashMap(List<Review> reviewsList, ArrayList<Business> businessList) {
 
         Map<String, Integer> toReturn = new HashMap<>();
@@ -24,23 +27,25 @@ public class step2 {
                 if (!(business.getBusinessId().equals(review.getBusinessId()))) {
                     continue;
                 }
-
                 // if review is sus, add it to the map
                 if (isSus(review)) {
-                    if (toReturn.contains(review)) {
-                        Integer x = toReturn.get(review);
-                        toReturn.put(review.getUserId(), x + 1);
+                    String userID = review.getUserId();
+                    if (toReturn.containsKey(userID)) {
+                        Integer x = toReturn.get(userID);
+                        toReturn.put(userID, x + 1);
                     } else {
-                        toReturn.put(review.getUserId(), 1);
+                        toReturn.put(userID, 1);
                     }
                 }
             }
         }
+        return toReturn;
     }
 
-    public ArrayList<Business> ArrayListToArrayList(List<Review> reviewsList, ArrayList<Business> businessList) {
+    // Returns ArrayList<String>
+    public ArrayList<String> ArrayListToArrayList(List<Review> reviewsList, ArrayList<Business> businessList) {
 
-        Map<String, Integer> toReturn = new HashMap<>();
+        ArrayList<String> toReturn = new ArrayList<>();
         for (Review review : reviewsList) {
             for (Business business : businessList) {
                 // if review not in businessList, skip
@@ -48,49 +53,79 @@ public class step2 {
                     continue;
                 }
 
-                // if review is sus, add it to the map
+                // if review is sus, add it to the new arraylist
                 if (isSus(review)) {
-                    if (toReturn.contains(review)) {
-                        Integer x = toReturn.get(review);
-                        toReturn.put(review.getUserId(), x + 1);
-                    } else {
-                        toReturn.put(review.getUserId(), 1);
+                    
+                    // check if already contains
+                    boolean contains = false;
+                    for (int i = 0; i < toReturn.size(); i+=2) {
+                        if (toReturn.get(i).equals(review.getUserId())) {
+                            int oldValue = Integer.parseInt(toReturn.get(i+1));
+                            toReturn.set(i+1, Integer.toString(oldValue+1));
+                            contains = true;
+                        }
+                    }
+                    if(!contains) {
+                        toReturn.add(review.getUserId());
+                        toReturn.add("1");
                     }
                 }
             }
         }
+        return toReturn;
     }
-
-    // Returns Map<userID, numOfSusReviews>
-    public Map<String, Integer> LinkedListToHashMap(List<Review> reviewsList, LinkedList<Business> businessList) {
+    
+    public Map<String, Integer> LinkedListToHashMap(List<Review> reviewsList, SinglyLinkedList<Business> businessList) {
 
         Map<String, Integer> toReturn = new HashMap<>();
         for (Review review : reviewsList) {
-            Node<E> current = businessList;
-            while (current != null) {
-                // if review not in businessList, skip
-                if (!(current.getElement().getBusinessId.equals(review.getBusinessId()))) {
-                    current = current.getNext();
-                    continue;
-                }
-
-                // if review is sus, add it to the map
-                if (isSus(review)) {
-                    if (toReturn.contains(review)) {
-                        Integer x = toReturn.get(review);
-                        toReturn.put(review.getUserId(), x + 1);
-                    } else {
-                        toReturn.put(review.getUserId(), 1);
-                    }
-                }
-
-                current = current.getNext();
-            }
+            Business current = businessList.first();
         }
+        return toReturn;
     }
 
-    
+    // Returns Map<userID, numOfSusReviews>
+    // public Map<String, Integer> LinkedListToHashMap(List<Review> reviewsList, LinkedList<Business> businessList) {
 
+    //     Map<String, Integer> toReturn = new HashMap<>();
+    //     for (Review review : reviewsList) {
+    //         Node current = businessList;
+    //         while (current != null) {
+    //             // if review not in businessList, skip
+    //             if (!(current.getElement().getBusinessId.equals(review.getBusinessId()))) {
+    //                 current = current.getNext();
+    //                 continue;
+    //             }
+
+    //             // if review is sus, add it to the map
+    //             if (isSus(review)) {
+    //                 String userID = review.getUserId();
+    //                 if (toReturn.containsKey(userID)) {
+    //                     Integer x = toReturn.get(userID);
+    //                     toReturn.put(userID, x + 1);
+    //                 } else {
+    //                     toReturn.put(userID, 1);
+    //                 }
+    //             }
+
+    //             current = current.getNext();
+    //         }
+    //     }
+    //     return toReturn;
+    // }
+    
+    // public ArrayList<String> LinkedListToArrayList(List<Review> reviewsList, LinkedList<Business> businessList) {
+
+    //     ArrayList<String> toReturn = new ArrayList<>();
+    //     for (Review review : reviewsList) {
+    //         Node current = businessList;
+    //         while (current != null) {
+
+    //             current = current.getNext();
+    //         }
+    //     }
+    //     return toReturn;
+    // }
     public static boolean isSus(Review review) {
         int reviewLength = review.getText().length();
         int reviewStars = review.getStars();
---
+++ step2test.java
@@ -0,0 +1,53 @@
+import entity.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class step2test {
+    public static void main(String[] args){
+        step2 step2 = new step2();
+        ArrayList<Business> bList = new ArrayList<Business>();
+        List<Review> reviewsList = new ArrayList<>();
+
+
+        System.out.println("a2a result: " + a2aTest(step2, bList, reviewsList));
+        bList.clear(); reviewsList.clear();
+
+        System.out.println("a2h result: " + a2hTest(step2, bList, reviewsList));
+        bList.clear(); reviewsList.clear();
+        
+    }
+
+    public static String a2aTest(step2 step2, ArrayList<Business> bList, List<Review> reviewsList) {
+        Review r1 = new Review("rID", "uID", "bID", 1, "h");
+        Review r2 = new Review("rID2", "uID", "bID", 1, "i");
+        Review r3 = new Review("rID3", "uID", "bID", 1, "j");
+        Review r4 = new Review("rID4", "uID2", "bID", 1, "k");
+        reviewsList.add(r1);
+        reviewsList.add(r2);
+        reviewsList.add(r3);
+        reviewsList.add(r4);
+        Business b1 = new Business("bID", "pizzhut", 1, 1, "Restaurant");
+        bList.add(b1);
+
+        ArrayList<String> result = step2.ArrayListToArrayList(reviewsList, bList);
+        return result.toString();
+    }
+
+    public static String a2hTest(step2 step2, ArrayList<Business> bList, List<Review> reviewsList) {
+        Review r1 = new Review("rID", "uID", "bID", 1, "h");
+        Review r2 = new Review("rID2", "uID", "bID", 1, "i");
+        Review r3 = new Review("rID3", "uID", "bID", 1, "j");
+        Review r4 = new Review("rID4", "uID2", "bID", 1, "k");
+        reviewsList.add(r1);
+        reviewsList.add(r2);
+        reviewsList.add(r3);
+        reviewsList.add(r4);
+        Business b1 = new Business("bID", "pizzhut", 1, 1, "Restaurant");
+        bList.add(b1);
+
+        Map<String, Integer> result = step2.ArrayListToHashMap(reviewsList, bList);
+        return result.toString();
+    }
+}
