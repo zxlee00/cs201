b601a7f2ff7231bd4234319f9403858e8eebb284
ChongJun123 <cjchew.2020@scis.smu.edu.sg>
2021-11-01 13:30:44 +0800

    completed step2 and tests

--- step2.java
+++ step2.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
-
+import java.util.TreeSet;
 import java.util.List;
 
 import entity.Business;
@@ -76,13 +76,13 @@ public class step2 {
     }
     
     public Map<String, Integer> LinkedListToHashMap(List<Review> reviewsList, SinglyLinkedList<Business> businessList) {
-
         Map<String, Integer> toReturn = new HashMap<>();
         for (Review review : reviewsList) {
             SinglyLinkedList.Node<Business> current = businessList.getHead();
             while(current != null) {
                 // if review not in businessList, skip
                 if (!(current.getElement().getBusinessId().equals(review.getBusinessId()))) {
+                    current = current.getNext();
                     continue;
                 }
                 // if review is sus, add it to the map
@@ -101,18 +101,93 @@ public class step2 {
         return toReturn;
     }
     
-    // public ArrayList<String> LinkedListToArrayList(List<Review> reviewsList, LinkedList<Business> businessList) {
+    public ArrayList<String> LinkedListToArrayList(List<Review> reviewsList, SinglyLinkedList<Business> businessList) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        for (Review review : reviewsList) {
+            SinglyLinkedList.Node<Business> current = businessList.getHead();
+            while(current != null) {
+                // if review not in businessList, skip
+                if (!(current.getElement().getBusinessId().equals(review.getBusinessId()))) {
+                    current = current.getNext();
+                    continue;
+                }
+                // if review is sus, add it to the new arraylist
+                if (isSus(review)) {
+                    
+                    // check if already contains
+                    boolean contains = false;
+                    for (int i = 0; i < toReturn.size(); i+=2) {
+                        if (toReturn.get(i).equals(review.getUserId())) {
+                            int oldValue = Integer.parseInt(toReturn.get(i+1));
+                            toReturn.set(i+1, Integer.toString(oldValue+1));
+                            contains = true;
+                        }
+                    }
+                    if(!contains) {
+                        toReturn.add(review.getUserId());
+                        toReturn.add("1");
+                    }
+                }
+                current = current.getNext();
+            }
+        }
+        return toReturn;
+    }
+    
+    public ArrayList<String> binaryTreeToArrayList(List<Review> reviewsList, TreeSet<Business> businessList) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        for (Review review : reviewsList) {
+            for (Business business : businessList) {
+                // if review not in businessList, skip
+                if (!(business.getBusinessId().equals(review.getBusinessId()))) {
+                    continue;
+                }
 
-    //     ArrayList<String> toReturn = new ArrayList<>();
-    //     for (Review review : reviewsList) {
-    //         Node current = businessList;
-    //         while (current != null) {
+                // if review is sus, add it to the new arraylist
+                if (isSus(review)) {
+                    
+                    // check if already contains
+                    boolean contains = false;
+                    for (int i = 0; i < toReturn.size(); i+=2) {
+                        if (toReturn.get(i).equals(review.getUserId())) {
+                            int oldValue = Integer.parseInt(toReturn.get(i+1));
+                            toReturn.set(i+1, Integer.toString(oldValue+1));
+                            contains = true;
+                        }
+                    }
+                    if(!contains) {
+                        toReturn.add(review.getUserId());
+                        toReturn.add("1");
+                    }
+                }
+            }
+        }
+        return toReturn;
+    }
 
-    //             current = current.getNext();
-    //         }
-    //     }
-    //     return toReturn;
-    // }
+    public Map<String, Integer> BinaryTreeToHashMap(List<Review> reviewsList, TreeSet<Business> businessList) {
+        Map<String, Integer> toReturn = new HashMap<>();
+        for (Review review : reviewsList) {
+            for (Business business : businessList) {
+                // if review not in businessList, skip
+                if (!(business.getBusinessId().equals(review.getBusinessId()))) {
+                    continue;
+                }
+                // if review is sus, add it to the map
+                if (isSus(review)) {
+                    String userID = review.getUserId();
+                    if (toReturn.containsKey(userID)) {
+                        Integer x = toReturn.get(userID);
+                        toReturn.put(userID, x + 1);
+                    } else {
+                        toReturn.put(userID, 1);
+                    }
+                }
+            }
+        }
+        return toReturn;
+    }
+
     public static boolean isSus(Review review) {
         int reviewLength = review.getText().length();
         int reviewStars = review.getStars();
